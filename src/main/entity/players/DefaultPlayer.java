f35245d203a4d743271ed6e4427b83f6d19c3400
zhaosh84 <wdebgr.zhao@mail.utoronto.ca>
2025-07-31 03:13:20 -0400

    added but/sell property use case.

--- src/main/entity/players/DefaultPlayer.java
+++ src/main/entity/players/DefaultPlayer.java
@@ -9,22 +9,9 @@ import java.awt.*;
 import static main.Constants.Constants.DEFAULT_INIT_MONEY;
 
 public class DefaultPlayer extends Player {
-    private String name;
 
     public DefaultPlayer(String name, Color color) {
         super(name, DEFAULT_INIT_MONEY, color);
         this.loadPortrait("main/Resources/default portrait.png");
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public float getMoney() {
-        return this.money;
-    }
-
-    public int getPosition() {
-        return this.position;
     }
 }
