f28019b874ec9ff1412ea6c91976e357525066eb
JimmyWooHoo <songxuanwu6@gmail.com>
2025-08-07 15:30:17 -0400

    fixed DicePresenter.java

--- src/main/interface_adapter/dice/DicePresenter.java
+++ src/main/interface_adapter/dice/DicePresenter.java
@@ -3,6 +3,13 @@ package main.interface_adapter.dice;
 import main.use_case.Dice.RollDice;
 
 public class DicePresenter {
+
+    /**
+     * Transforms the given dice roll result into a view model.
+     *
+     * @param diceResult the result of a dice roll, must not be null
+     * @return a {@link DiceViewModel} populated with the two dice values and their sum
+     */
     public DiceViewModel execute(RollDice.DiceResult diceResult) {
         return new DiceViewModel(
                 diceResult.getDice1(),
