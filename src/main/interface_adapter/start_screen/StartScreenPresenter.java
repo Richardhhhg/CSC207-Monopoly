72a8e39b3c4e0a4b554f941db047bf2a72094cb8
JimmyWooHoo <songxuanwu6@gmail.com>
2025-08-07 16:18:22 -0400

    fixed StartScreenPresenter.java

--- src/main/interface_adapter/start_screen/StartScreenPresenter.java
+++ src/main/interface_adapter/start_screen/StartScreenPresenter.java
@@ -1,8 +1,15 @@
-package main.interface_adapter.StartScreen;
+package main.interface_adapter.start_screen;
 
 import main.use_case.StartScreen.StartGame;
 
 public class StartScreenPresenter {
+
+    /**
+     * Transforms the game-start result into a view model.
+     *
+     * @param result the result of the start-game use case, must not be null
+     * @return a {@link StartScreenViewModel} populated with welcome message, rules, and button labels
+     */
     public StartScreenViewModel execute(StartGame.StartGameResult result) {
         return new StartScreenViewModel(
                 result.getWelcomeMessage(),
