b5b0cfe816e335d9838bfde238684dcebec2a577
Richard Hong <richardh110604@gmail.com>
2025-08-07 12:37:37 -0400

    Fixed some weird imports with constants being lowercase

--- src/main/app/Main.java
+++ src/main/app/Main.java
@@ -3,11 +3,18 @@ package main.app;
 import main.view.GameView;
 import main.view.StartScreen;
 
-public class Main{
+public class Main {
+    /**
+     * Main method to start the application.
+     * @param args
+     */
     public static void main(String[] args) {
         new StartScreen();
     }
 
+    /**
+     *
+     */
     public static void startGame() {
         new GameView();
     }
