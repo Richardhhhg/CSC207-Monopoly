1a632affdda70f5fdb7f971f946291d42244e502
Nate Y <nate@yoon.me>
2025-08-08 00:11:54 -0400

    Read property names and prices from json file

---
+++ src/main/infrastructure/FallbackPropertyDataSource.java
@@ -0,0 +1,32 @@
+package main.infrastructure;
+
+import main.use_case.Game.PropertyDataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Fallback implementation that provides hardcoded property data.
+ * Used when primary data source fails.
+ */
+public class FallbackPropertyDataSource implements PropertyDataSource {
+
+    @Override
+    public List<PropertyInfo> getPropertyData() {
+        List<PropertyInfo> propertyData = new ArrayList<>();
+        String[] fallbackNames = {
+            "Mediterranean Ave", "Baltic Ave", "Oriental Ave", "Vermont Ave",
+            "Connecticut Ave", "St. James Place", "Tennessee Ave", "New York Ave",
+            "Kentucky Ave", "Indiana Ave", "Illinois Ave", "Atlantic Ave",
+            "Ventnor Ave", "Marvin Gardens", "Pacific Ave", "North Carolina Ave",
+            "Pennsylvania Ave", "Boardwalk", "Park Place", "Luxury Tax",
+            "Short Line", "B&O Railroad", "Reading Railroad", "Pennsylvania Railroad"
+        };
+
+        for (int i = 0; i < fallbackNames.length; i++) {
+            int price = 60 + i * 20;
+            propertyData.add(new PropertyInfo(fallbackNames[i], Math.min(price, 400)));
+        }
+
+        return propertyData;
+    }
+}
